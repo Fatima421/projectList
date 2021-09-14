6fac9f21cd2ba90bfc008fbef41de81c86eb8c4d
Fatima Sahar <fatima.sahar@basetis.com>
2021-09-14 11:11:16 +0200

    Initial Commit

---
+++ ProjectList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectList
+//
+//  Created by Fatima Syed on 14/09/2021.
+//  Copyright © 2021 Fatima Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
